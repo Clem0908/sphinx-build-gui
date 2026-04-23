27298605847e71bee336221083ce53ee7b739352
clement.lyonnet <clement.lyonnet@gatewatcher.com>
2026-04-23 20:51:01 +0200

    version 1.2.8: app icon

--- src/main/java/org/clem0908/sphinxbuildgui/Main.java
+++ src/main/java/org/clem0908/sphinxbuildgui/Main.java
@@ -1,6 +1,7 @@
 package org.clem0908.sphinxbuildgui;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
@@ -14,6 +15,7 @@ public class Main extends Application {
         String version = Main.class.getPackage().getImplementationVersion();
         stage.setTitle("Sphinx Build GUI" + (version != null ? " " + version : ""));
         scene.getStylesheets().add(Main.class.getResource("style.css").toExternalForm());
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("sphinx_build_gui_icon.png")));
         stage.setScene(scene);
         stage.show();
     }
